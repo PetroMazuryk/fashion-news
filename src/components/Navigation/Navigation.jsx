2d6852e26437b02151f582e8c986bdeec31c52d0
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2025-04-20 01:21:45 +0300

    add styles to the Fashion News button

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -5,9 +5,7 @@ import css from './Navigation.module.css';
 import RippleButton from '../RippleButton/RippleButton';
 export const Navigation = () => {
   const { isLoggedIn } = useAuth();
-  const buildLinkClass = ({ isActive }) => {
-    return clsx(css.link, isActive && css.active);
-  };
+
   return (
     <nav className={css.nav}>
       <NavLink to="/" className={css.link}>
@@ -19,8 +17,15 @@ export const Navigation = () => {
       </NavLink>
 
       {isLoggedIn && (
-        <NavLink to="/fashion-news" className={buildLinkClass}>
-          Fashion News
+        <NavLink to="/fashion-news" className={css.link}>
+          {({ isActive }) => (
+            <RippleButton
+              isActive={isActive}
+              className={clsx(css.rippleButton)}
+            >
+              Fashion News
+            </RippleButton>
+          )}
         </NavLink>
       )}
     </nav>
