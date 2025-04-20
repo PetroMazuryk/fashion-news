1d48186cdd5785a705d4fdfa0059366c70ed86a8
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2025-04-20 23:27:34 +0300

    add FavoriteNewsPage

--- src/pages/FavoriteNewsPage/FavoriteNewsPage.jsx
+++ src/pages/FavoriteNewsPage/FavoriteNewsPage.jsx
@@ -1,5 +1,23 @@
+import { useSelector } from 'react-redux';
+import { selectAllNews } from '../../redux/contacts/selectors';
+import { Section } from '../../components/Section/Section';
+import FashionNews from '../../components/FashionNews/FashionNews';
+import { DocumentTitle } from '../../components/DocumentTitle';
+
 const FavoriteNewsPage = () => {
-  return <div>FavoriteNewsPage</div>;
+  const news = useSelector(selectAllNews);
+  const favoriteNews = news.filter(item => item.favorite);
+
+  return (
+    <>
+      <DocumentTitle>Favorite fashion news</DocumentTitle>
+      <Section title="List of favorite fashion news">
+        {favoriteNews.map(item => (
+          <FashionNews key={item._id} contact={item} />
+        ))}
+      </Section>
+    </>
+  );
 };
 
 export default FavoriteNewsPage;
